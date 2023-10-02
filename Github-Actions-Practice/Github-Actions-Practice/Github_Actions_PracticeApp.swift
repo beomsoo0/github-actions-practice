ed25a33804291afb848f0118e55f9ce3ecf64c58
김범수[Mobile Core Platform] <beomsoo@navercorp.com>
2023-10-02 17:34:49 +0900

    init project

---
+++ Github-Actions-Practice/Github-Actions-Practice/Github_Actions_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Actions_PracticeApp.swift
+//  Github-Actions-Practice
+//
+//  Created by beomsoo-kim on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct Github_Actions_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
